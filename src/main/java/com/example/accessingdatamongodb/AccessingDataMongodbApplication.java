81e9d27e0870ddcef6dcb31f365f19a44fd3f5e0
segovelo <gauchosinpingo@gmail.com>
2022-10-08 12:59:08 +0100

    Adding logging configuration.

--- src/main/java/com/example/accessingdatamongodb/AccessingDataMongodbApplication.java
+++ src/main/java/com/example/accessingdatamongodb/AccessingDataMongodbApplication.java
@@ -1,18 +1,33 @@
 package com.example.accessingdatamongodb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class AccessingDataMongodbApplication implements CommandLineRunner {
 
+  private static Logger logger = LoggerFactory.getLogger(AccessingDataMongodbApplication.class);
   @Autowired
   private CustomerRepository repository;
 
   public static void main(String[] args) {
-    SpringApplication.run(AccessingDataMongodbApplication.class, args);
+	  try {
+	  ApplicationContext ctx = SpringApplication.run(AccessingDataMongodbApplication.class, args);
+	  Environment environment = ctx.getEnvironment();
+	  
+      logger.info("Accessing-Data-MongoDB Application started successfully !");
+      logger.info(
+          "Application listening on port: {}", environment.getProperty("local.server.port"));
+    } catch (Exception ex) {
+      logger.error("Accessing-Data-MongoDB Application failed :( *** ", ex);
+      throw new RuntimeException();
+    }
   }
 
   @Override
---
+++ src/test/java/com/example/accessingdatamongodb/AccessingDataMongodbApplicationTest.java
@@ -0,0 +1,25 @@
+package com.example.accessingdatamongodb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/** 
+* 20 Dec 2021 00:23:31
+* @Javadoc TODO 
+*
+* @author Sebastian Vergara Losada  **/
+
+class AccessingDataMongodbApplicationTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRun() {
+		fail("Not yet implemented");
+	}
+
+}
